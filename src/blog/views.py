09f94722f2cf4f277e2887869d7d2faa65d9a924
karatugo <karatugo@gmail.com>
2020-01-22 04:45:03 -0800

    Add step 25 - QuerySet Lookups

--- src/blog/views.py
+++ src/blog/views.py
@@ -5,8 +5,11 @@ from django.shortcuts import render, get_object_or_404
 from .models import BlogPost
 
 def blog_post_detail_page(request, slug):
-    obj = get_object_or_404(BlogPost, slug=slug)
-    # obj = BlogPost.objects.get(slug=slug)
+    queryset = BlogPost.objects.filter(slug=slug)
+    if queryset.count() >= 1:
+        obj = queryset.first()
+        
+    # obj = get_object_or_404(BlogPost, slug=slug)
     template_name = "blog_post_detail.html"
     context = {"object":obj}
     return render(request, template_name, context)
