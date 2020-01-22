0f6450779c1597cef9e2c00cdcf5eb82c194c421
karatugo <karatugo@gmail.com>
2020-01-22 05:53:05 -0800

    Add step 27 - CRUD & Views

--- src/blog/models.py
+++ src/blog/models.py
@@ -5,5 +5,5 @@ from django.db import models
 class BlogPost(models.Model):
     # id = models.IntegerField() # primary key
     title = models.TextField()
-    slug = models.SlugField() # hello world -> hello-world
+    slug = models.SlugField(unique=True) # hello world -> hello-world
     content = models.TextField(null=True, blank=True)
--- src/blog/views.py
+++ src/blog/views.py
@@ -5,11 +5,30 @@ from django.shortcuts import render, get_object_or_404
 from .models import BlogPost
 
 def blog_post_detail_page(request, slug):
-    queryset = BlogPost.objects.filter(slug=slug)
-    if queryset.count() >= 1:
-        obj = queryset.first()
-        
-    # obj = get_object_or_404(BlogPost, slug=slug)
+    #print("DJANGO SAYS", request.method, request.path, request.user)
+    ## When slug was non-unique
+    # queryset = BlogPost.objects.filter(slug=slug)
+    # if queryset.count() == 0:
+    #     raise Http404    
+    # obj = queryset.first()
+
+    obj = get_object_or_404(BlogPost, slug=slug)
     template_name = "blog_post_detail.html"
     context = {"object":obj}
     return render(request, template_name, context)
+
+def blog_post_list_view(request):
+    return
+
+## C.R.U.D. operations
+def blog_post_create_view(request):
+    return
+
+def blog_post_retrieve_view(request):
+    return
+
+def blog_post_update_view(request):
+    return
+
+def blog_post_delete_view(request):
+    return
