197b8737e3a465ccd23848135f7454c6b5851e31
karatugo <karatugo@gmail.com>
2020-01-23 16:37:52 +0300

    Add step 39 - Associate Blog Post to a User with Foreign Keys

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,9 +1,25 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+User = settings.AUTH_USER_MODEL
 
-class BlogPost(models.Model):
+class BlogPost(models.Model): #blogpost_set -> queryset
     # id = models.IntegerField() # primary key
+    user = models.ForeignKey(User, default=1, null=True, on_delete=models.SET_NULL)
     title = models.CharField(max_length=120)
     slug = models.SlugField(unique=True) # hello world -> hello-world
     content = models.TextField(null=True, blank=True)
+
+    # >>> from django.contrib.auth import get_user_model
+    # >>> User = get_user_model()
+    # >>> User
+    # <class 'django.contrib.auth.models.User'>
+    # >>> User.objects.first().blogpost_set.all()
+    # <QuerySet [<BlogPost: BlogPost object (1)>, <BlogPost: BlogPost object (2)>, <BlogPost: BlogPost object (3)>, <BlogPost: BlogPost object (4)>, <BlogPost: BlogPost object (5)>, <BlogPost: BlogPost object (6)>, <BlogPost: BlogPost object (7)>, <BlogPost: BlogPost object (8)>, <BlogPost: BlogPost object (9)>, <BlogPost: BlogPost object (10)>, <BlogPost: BlogPost object (11)>, <BlogPost: BlogPost object (12)>, <BlogPost: BlogPost object (13)>, <BlogPost: BlogPost object (14)>, <BlogPost: BlogPost object (15)>]>
+    #
+    # OR:
+    #
+    # >>> from blog.models import BlogPost
+    # >>> qs = BlogPost.objects.filter(user__id=1)
+    
