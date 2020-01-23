ae355e75de4ed1fa9581e445bef9413a7c8d5dba
karatugo <karatugo@gmail.com>
2020-01-23 16:18:35 +0300

    Add step 37 - Validate Data on Fields

--- src/blog/forms.py
+++ src/blog/forms.py
@@ -11,3 +11,12 @@ class BlogPostModelForm(forms.ModelForm):
     class Meta:
         model = BlogPost
         fields = ["title", "slug", "content"]
+
+    def clean_title(self, *args, **kwargs):
+        title = self.cleaned_data.get("title")
+        qs = BlogPost.objects.filter(title__iexact=title)
+        print(qs)
+        if qs.exists():
+            raise forms.ValidationError("This title is in use")
+
+        return title
