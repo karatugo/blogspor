2dc9743c3f4062d953afa528bc5f07377873285f
karatugo <karatugo@gmail.com>
2020-01-24 15:19:17 +0300

    Add step 51 - Static Files and Uploading Files

--- src/blog/models.py
+++ src/blog/models.py
@@ -21,6 +21,7 @@ class BlogPostManager(models.Manager):
 class BlogPost(models.Model): #blogpost_set -> queryset
     # id = models.IntegerField() # primary key
     user = models.ForeignKey(User, default=1, null=True, on_delete=models.SET_NULL)
+    image = models.FileField(upload_to="image/", blank=True, null=True)
     title = models.CharField(max_length=120)
     slug = models.SlugField(unique=True) # hello world -> hello-world
     content = models.TextField(null=True, blank=True)
