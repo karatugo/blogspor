dafe6e434c407cdb9496e786dc84a0c5ef3f7ada
karatugo <karatugo@gmail.com>
2020-01-24 10:43:51 +0300

    Add step 43 - Delete and Confirm

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.admin.views.decorators import staff_member_required
 from django.http import Http404
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 # Create your views here.
 from .forms import BlogPostModelForm
@@ -37,6 +37,7 @@ def blog_post_detail_view(request, slug):
     context = {"object": obj}
     return render(request, template_name, context)
 
+@staff_member_required
 def blog_post_update_view(request, slug):
     obj = get_object_or_404(BlogPost, slug=slug)
     form = BlogPostModelForm(request.POST or None, instance=obj)
@@ -44,11 +45,20 @@ def blog_post_update_view(request, slug):
         form.save()
     
     template_name = "form.html"
-    context = {"form": form, "title": "Update {}".format(obj.title)}
+    context = {
+        "title": "Update {}".format(obj.title), 
+        "form": form
+    }
+
     return render(request, template_name, context)
 
+@staff_member_required
 def blog_post_delete_view(request, slug):
     obj = get_object_or_404(BlogPost, slug=slug)
     template_name = "blog/delete.html"
+    if request.method == "POST":
+        obj.delete()
+        return redirect("/blog")
+
     context = {"object": obj}
     return render(request, template_name, context)
